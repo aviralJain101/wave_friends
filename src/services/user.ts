f894f1bf81bf8dd428175ec93d6592a0790f170d
Aviral Jain <48090218+aviralJain101@users.noreply.github.com>
2023-04-08 20:01:03 +0000

    getUserFromUserId function in the export object

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,17 +1,15 @@
 import User from '../models/user';
 import IUser from '../interfaces/user';
 
-const getUserFromUserId = async function (userId: string): Promise<IUser | null> {
-    try {
-        const user = await User.findById(userId);
-
-        return user;
-    } catch (err) {
-        console.error(err);
-        return null;
-    }
-};
-
 export default {
-    getUserFromUserId
+    getUserFromUserId: async function (userId: string): Promise<IUser | null> {
+        try {
+            const user = await User.findById(userId);
+
+            return user;
+        } catch (err) {
+            console.error(err);
+            return null;
+        }
+    },
 }
